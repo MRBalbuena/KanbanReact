96e0cceef7d53006d3ab52baeff1bae28bbb3f22
mrbalbuena <mrbalbuena@hotmail.com>
2017-03-22 22:07:36 +0000

    Implemented call to github repos

--- src/components/menu/Repos.tsx
+++ src/components/menu/Repos.tsx
@@ -1,10 +1,40 @@
 import * as React  from 'react';
+import * as reqPromise from 'request-promise';
 
-export class Repos extends React.Component<{},{}> {
+interface IReposState {
+  repositories: any[];
+}
+
+export class Repos extends React.Component<{}, IReposState> {
+  public state: IReposState
+  constructor(){
+    super();
+    this.state = {
+      repositories: []
+    }
+  }
+
+  componentDidMount(){
+    let req = {
+      uri: 'https://api.github.com/users/pro-react/repos',
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+      json: true
+    };
+    reqPromise(req).then((responseData: any) => {
+      console.log(responseData);
+      this.setState({repositories: responseData});
+    })
+  }
+
   render() {
+    let repos = this.state.repositories.map((repo: any) => {
+      return <li key={repo.id}>{repo.name}</li>
+    });
     return (
       <div>
         <h1>GitHub Repos</h1>
+        <ul>{repos}</ul>
       </div>
     );
   }
